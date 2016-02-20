37681bf72193d7dda1cede7d4a988f9cc44e2db8
Iulian Tabără <itabara@live.com>
2016-02-20 10:08:50 +0200

    First commit

---
+++ asp-reactJS/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asp_reactJS.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult helloWorld()
+        {
+            return new JsonResult
+            {
+                Data = "Hello World. Reply from server side!",
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+    }
+}
